9cadb3485be352be5ad18ab1341bce6aed73d501
Cezary Olborski <cezary.olborski@gmail.com>
2025-01-04 09:38:39 +0800

    feat: CVS Exporter

--- src/domain/transaction_record.rs
+++ src/domain/transaction_record.rs
@@ -1,5 +1,5 @@
+use std::fmt;
 use serde::Serialize;
-use log::{info, warn};
 
 #[derive(Serialize)]
 pub struct TransactionRecord {
@@ -15,27 +15,28 @@ pub struct TransactionRecord {
     pub fee_currency: String,
 }
 
-use std::fmt;
-
 impl fmt::Display for TransactionRecord {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "Transaction Record:")?;
-        writeln!(f, "  Date: {}", self.date)?;
-        writeln!(f, "  Tx Hash: {}", self.tx_hash)?;
-        writeln!(f, "  Source: {}", self.tx_src)?;
-        writeln!(f, "  Destination: {}", self.tx_dest)?;
-        writeln!(f, "  Sent Amount: {} {}",
-                 self.sent_amount.map_or("N/A".to_string(), |amt| amt.to_string()),
-                 self.sent_currency.as_deref().unwrap_or("N/A")
-        )?;
-        writeln!(f, "  Received Amount: {} {}",
-                 self.received_amount.map_or("N/A".to_string(), |amt| amt.to_string()),
-                 self.received_currency.as_deref().unwrap_or("N/A")
-        )?;
-        writeln!(f, "  Fee: {} {}",
-                 self.fee_amount.to_string(),
-                 self.fee_currency
-        )?;
+    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        log::info!("Transaction Record:");
+        log::info!("  Date: {}", self.date);
+        log::info!("  Tx Hash: {}", self.tx_hash);
+        log::info!("  Source: {}", self.tx_src);
+        log::info!("  Destination: {}", self.tx_dest);
+        log::info!(
+            "  Sent Amount: {} {}",
+            self.sent_amount.map_or("N/A".to_string(), |amt| amt.to_string()),
+            self.sent_currency.as_deref().unwrap_or("N/A")
+        );
+        log::info!(
+            "  Received Amount: {} {}",
+            self.received_amount.map_or("N/A".to_string(), |amt| amt.to_string()),
+            self.received_currency.as_deref().unwrap_or("N/A")
+        );
+        log::info!(
+            "  Fee: {} {}",
+            self.fee_amount.to_string(),
+            self.fee_currency
+        );
         Ok(())
     }
 }
--- src/ports/mod.rs
+++ src/ports/mod.rs
@@ -1,2 +1,5 @@
 pub mod solana_th_service;
+pub mod file_exporter_service;
+
 pub use solana_th_service::SolanaTHService;
+pub use file_exporter_service::FileExporterService;
